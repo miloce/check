3fb8ce03eb10f07fbe7730d6e61a3291d6d2612d
miloce <admim@luozhinet.com>
2022-08-27 19:43:53 +0800

    check

---
+++ js/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Miloce 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1735047-6f378fbcc199a76ccb9b72c2',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://www.luozhinet.com/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.luozhinet.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/miloce/'
+    },
+  ],
+};
